b194c0db254dce879c31cc55b562c21720834295
vmelo.gon@gmail.com <vmelo.gon@gmail.com>
2022-11-30 17:12:26 -0300

    Tratamento de erros em create_source_file; não testado.

--- aush.sh
+++ aush.sh
@@ -2,36 +2,50 @@
 
 create_source_file ()
 {
-  lib_dir="$(dirname \"$script_file\")"
-  source_file="${lib_dir}/lib/aush.sh"
+  local source_file lib_dir
+  source_file="$(dirname $script_file)/lib/aush_source.sh"
+  lib_dir="$(dirname $source_file)"
   [ ! -d "$lib_dir" ] && mkdir "$lib_dir"
   cat << 'EOF' > "$source_file"
 aush_update ()
 {
-local scargs
-script_file="$1"
-git_file="$2"
-script_args="$3"
 if [ -z "$AUSH_UPDATE" ]; then
-  export AUSH_UPDATE=1
-  cp "${aush_script_file}" "${aush_script_file}".bak
-  "${aush_git_file}" "$@" &
-  exit 0
+export AUSH_UPDATE=1
+# add this source to the updated file if it it is not 'aushed'
+if [ ! -s "$(dirname ${aush_updated_script_file})/lib/aush_source.sh" ]
+aush "${aush_updated_script_file}"
+fi
+aush "${aush_updated_script_file}"
+"${aush_updated_script_file}" "$@" &
+exit 0
 else
-  cp "${aush_git_file}" "${aush_script_file}"
+cp "${aush_updated_script_file}" "${aush_aushed_script_file}"
 fi
 }
 
+aush_aushed_script_basename="$(basename $0)"
+aush_aushed_script_file="$(pwd)"/"$aush_aushed_script_basename"
+aush_gh_repo="${aush_aushed_script_basename%.*}"
+printf 'aush: checking updates for %s from %s repo' "$aush_aushed_script_file" "$aush_gh_repo"
 cd /tmp
-aush_script_file=$(basename "$0")
-aush_repo="${aush_script_file%.*}"
-printf 'aush: updating %s from %s' "$aush_script_file" "$aush_repo"
-gh repo clone $aush_gh_repo && cd $aush_gh_repo || exit 1
-aush_git_file="$(pwd)"/"$aush_script_file"
-cmp --silent "$aush_script_file" "$aush_git_file"
+# checks for a repo with the same name of the script, no extension, on user account
+gh repo clone "$aush_gh_repo"
+if [ $? -eq 0 ]; then
+cd "$aush_gh_repo"
+# works for 'script', 'script.sh' and 'script.bash' from cloned repo...
+aush_updated_script_file="$(pwd)"/$(basename $(find . -maxdepth 1 -type f (-name "${aush_aushed_script_basename}" -o -name "${aush_aushed_script_basename}.sh" -o -name "${aush_aushed_script_basename}.bash"))
+if [ -z "$aush_updated_script_file" ]
+printf 'aush: could not update, error while checking downloaded script from repo; allowed extensions: .sh and .bash'
+else
+cmp --silent "$aush_aushed_script_file" "$aush_updated_script_file"
 if [ $? -eq 1 ]; then
 aush_update "$@"
 fi
+else
+printf 'aush: could not update, error while cloning repo; did you logged in with "gh auth login"?'
+fi
+fi
+cd $(dirname "$aush_aushed_script_file")
 EOF
   exit $?
 }
@@ -39,10 +53,10 @@ EOF
 add_source ()
 {
   local first_line
-  first_line=$(head -n1 "$script_file")
+  first_line="$(head -n1 $script_file)"
   if [ "$(echo "$first_line" | cut -c1-2)" = '#!' ]; then
     lang=$(echo "$first_line" | cut -c 3- | xargs basename)
-    if [ "$lang" = sh ] || [ "$lang" = bash ]; then
+    if [ "$lang" = 'sh' ] || [ "$lang" = 'bash' ]; then
       create_source_file
       # add source after shbang
       sed "1a\|\# aush must run before any other command\|\.\ $source_file" "$script_file" | tr '|' '\n' > "${script_file}.tmp" && mv "${script_file}.tmp" "$script_file"
@@ -73,23 +87,5 @@ if [ $# -ne 1 ]; then
   printf "aush [script|help]" && exit 1
 fi
 
-  script_file=$1
-if [ -s $1 ]; then
-  echo $1
-  add_source $1
-else
-  case "$1" in
-    update)
-      update_script $2 $3
-      exit 0
-      ;;
-    help)
-      echo help
-      exit 0
-      ;;
-    *)
-      echo 'please try again'
-      exit 1
-      ;;
-  esac
-fi
+script_file="$1"
+add_source
