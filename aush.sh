a73bb93a841264cbc4777efa8eeb28d288b06442
Vinicius Melo <vmelo.gon@gmail.com>
2022-12-01 16:54:29 -0300

    Corrigido erro no source ao buscar pelo arquivo baixado do repositório.

--- aush.sh
+++ aush.sh
@@ -2,93 +2,92 @@
 
 set_config_file ()
 {
-local aush_remote_git_account aush_config_file url
-aush_config_file="${HOME}/.aush"
+  local aush_remote_git_account aush_config_file url
+  aush_config_file="${HOME}/.aush"
+  while :; do
+    printf 'aush: please, enter the remote git account url (example: "https://github.com/username": '
+    read url
+    if curl --output /dev/null --silent --head --fail "$url"; then
+      break
+    else
+      printf 'aush: please enter a valid url\n'
+    fi
+  done
 
-while :; do
-printf 'aush: please, enter the remote git account url (example: "https://github.com/username": '
-read url
-if curl --output /dev/null --silent --head --fail "$url"; then
-  break
-else
-  printf 'aush: please enter a valid url\n'
-fi
-done
+  aush_remote_git_account="$url"
 
-aush_remote_git_account="$url"
-
-cat << EOF > "$aush_config_file"
+  cat << EOF > "$aush_config_file"
 # aush config file
 # see 'aush help' for info
 aush_remote_git_account=${aush_remote_git_account%/}
 EOF
 
-printf 'aush: configuration file generated succesfully ("%s")\n' "${HOME}/.aush"
+  printf 'aush: configuration file generated succesfully ("%s")\n' "${HOME}/.aush"
 
-return 0
+  return 0
 }
 
 create_source_file ()
 {
   local lib_dir
-  source_file='./lib/aush_source.sh'
+  source_file="$(dirname ${script_file})/lib/aush_source.sh"
   lib_dir="$(dirname $source_file)"
   [ ! -d "$lib_dir" ] && mkdir "$lib_dir"
-  cat << 'EOF' > "$source_file"
+  cat <<-'EOF' > "$source_file"
 aush_update ()
 {
-if [ -z "$AUSH_STATUS" ]; then
-cd "$AUSH_REPO"
-# works for 'script', 'script.sh' and 'script.bash' or any other ext - TODO: only .bash and .sh or no ext on find command -, from cloned repo...
-export AUSH_UPDATED_SCRIPT_FILE="$(pwd)"/"$(find * -maxdepth 1 -type f -name ${AUSH_REPO}*)"
-if [ -n "$AUSH_UPDATED_SCRIPT_FILE" ]; then
-if ! cmp --silent "$AUSH_ORIGINAL_SCRIPT_FILE" "$AUSH_UPDATED_SCRIPT_FILE"; then
-export AUSH_STATUS='updating'
-# add this source to the updated file if it is not 'aushed'
-[ ! -s "$(dirname ${AUSH_UPDATED_SCRIPT_FILE})/lib/aush_source.sh" ] && aush "$AUSH_UPDATED_SCRIPT_FILE"
-chmod +x "$AUSH_UPDATED_SCRIPT_FILE"
-"$AUSH_UPDATED_SCRIPT_FILE" "$@"
-exit 0
-else
-return 0
-fi
-else
-return 1
-fi
-else
-export AUSH_STATUS='done'
-cp "$AUSH_UPDATED_SCRIPT_FILE" "$AUSH_ORIGINAL_SCRIPT_FILE"
-cd $(dirname "$AUSH_ORIGINAL_SCRIPT_FILE")
-"$AUSH_ORIGINAL_SCRIPT_FILE" "$@"
-rm -rf $(dirname "$AUSH_UPDATED_SCRIPT_FILE")
-exit 0
-fi
+  if [ -z "$AUSH_STATUS" ]; then
+    local downloaded_script_file
+    cd "$AUSH_REPO"
+    downloaded_script_file="$(find * -maxdepth 1 -type f -name ${AUSH_REPO}*)"
+    if [ -n "$downloaded_script_file" ]; then
+      export AUSH_UPDATED_SCRIPT_FILE="$(pwd)"/"$downloaded_script_file"
+      if ! cmp --silent "$AUSH_ORIGINAL_SCRIPT_FILE" "$AUSH_UPDATED_SCRIPT_FILE"; then
+        export AUSH_STATUS='updating'
+        [ ! -s "$(dirname ${AUSH_UPDATED_SCRIPT_FILE})/lib/aush_source.sh" ] && aush "$AUSH_UPDATED_SCRIPT_FILE"
+        chmod +x "$AUSH_UPDATED_SCRIPT_FILE"
+        "$AUSH_UPDATED_SCRIPT_FILE" "$@"
+        exit 0
+      else
+        return 0
+      fi
+    else
+      return 1
+    fi
+  else
+    export AUSH_STATUS='done'
+    cp "$AUSH_UPDATED_SCRIPT_FILE" "$AUSH_ORIGINAL_SCRIPT_FILE"
+    cd $(dirname "$AUSH_ORIGINAL_SCRIPT_FILE")
+    "$AUSH_ORIGINAL_SCRIPT_FILE" "$@"
+    rm -rf $(dirname "$AUSH_UPDATED_SCRIPT_FILE")
+    exit 0
+  fi
 }
+
 if ! . "${HOME}/.aush"; then
-printf 'aush: could not read "${HOME}/.aush", please config aush with "aush config"\n'
+  printf 'aush: could not read "${HOME}/.aush", please config aush with "aush config"\n'
 else
-if [ -z "$AUSH_STATUS" ]; then
-aush_original_script_basename="$(basename $0)"
-export AUSH_REPO="${aush_original_script_basename%.*}"
-export AUSH_ORIGINAL_SCRIPT_FILE="$(pwd)"/"$aush_original_script_basename"
-printf 'aush: checking updates for "%s" from "%s" repo\n' "$AUSH_ORIGINAL_SCRIPT_FILE" "$AUSH_REPO"
-cd '/tmp'
-[ -d "$AUSH_REPO" ] && rm -fr "$AUSH_REPO"
-# checks for a repo with the same name of the script, no extension, on user account
-if git clone "$aush_remote_git_account"/"$AUSH_REPO".git 2>/dev/null; then
-if ! aush_update "$@"; then
-printf 'aush: could not update; could not find "%s", "%s.sh" or "%s.bash" on repo\n' "$AUSH_REPO" "$AUSH_REPO" "$AUSH_REPO"
-fi
-else
-printf 'aush: could not update; error while cloning "%s" repo from "%s" account (check if repo/account exist)\n' "$AUSH_REPO" "$AUSH_HTTPS_ACCOUNT"
-fi
-elif [ $AUSH_STATUS = 'updating' ]; then
-printf 'aush: updating "%s"\n' "$AUSH_ORIGINAL_SCRIPT_FILE"
-aush_update "$@"
-else
-printf 'aush: "%s" updated succesfully!\n' "$AUSH_ORIGINAL_SCRIPT_FILE"
-fi
-unset AUSH_STATUS AUSH_ORIGINAL_SCRIPT_FILE AUSH_UPDATED_SCRIPT_FILE
+  if [ -z "$AUSH_STATUS" ]; then
+    aush_original_script_basename="$(basename $0)"
+    export AUSH_REPO="${aush_original_script_basename%.*}"
+    export AUSH_ORIGINAL_SCRIPT_FILE="$(dirname $(which $0))"/"$aush_original_script_basename"
+    printf 'aush: checking updates for "%s" from "%s" repo\n' "$AUSH_ORIGINAL_SCRIPT_FILE" "$AUSH_REPO"
+    cd '/tmp'
+    [ -d "$AUSH_REPO" ] && rm -fr "$AUSH_REPO"
+    if git clone "$aush_remote_git_account"/"$AUSH_REPO".git 2>/dev/null; then
+      if ! aush_update "$@"; then
+        printf 'aush: could not update; could not find "%s", "%s.sh" or "%s.bash" on repo\n' "$AUSH_REPO" "$AUSH_REPO" "$AUSH_REPO"
+      fi
+    else
+      printf 'aush: could not update; error while cloning "%s" repo from "%s" account (check if repo/account exist)\n' "$AUSH_REPO" "$AUSH_HTTPS_ACCOUNT"
+    fi
+  elif [ $AUSH_STATUS = 'updating' ]; then
+    printf 'aush: updating "%s"\n' "$AUSH_ORIGINAL_SCRIPT_FILE"
+    aush_update "$@"
+  else
+    printf 'aush: "%s" updated succesfully!\n' "$AUSH_ORIGINAL_SCRIPT_FILE"
+  fi
+  unset AUSH_STATUS AUSH_ORIGINAL_SCRIPT_FILE AUSH_UPDATED_SCRIPT_FILE
 fi
 unset aush_remote_git_account
 echo
