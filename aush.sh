402b7436e27341dd937479c9c561f759f41cb6f2
Vinicius Melo <vmelo.gon@gmail.com>
2022-12-01 21:59:19 -0300

    Atualiza a linha de source no arquivo atualizado, para que não aponte para um 'aush_source.sh' em '/tmp'

--- aush.sh
+++ aush.sh
@@ -39,8 +39,10 @@ create_source_file ()
 
 add_source ()
 {
-  if grep 'aush_source.sh' $script_file >/dev/null; then
-    printf 'aush: "%s" already sources an aush source file; do not add duplicate to code\n' "$script_file"
+  if grep 'aush_source.sh' "$script_file" >/dev/null; then
+    printf 'aush: "%s" already sources an "aush_source.sh" file; updating source line\n' "$script_file"
+    sed -i "/aush\_source\.sh/d" "$script_file"
+    add_source && return 0
     if [ ! -s './lib/aush_source.sh' ]; then 
       printf 'aush: could not find a "./lib/aush_source.sh" relative to "%s"; creating it\n' "$script_file"
       if create_source_file; then
